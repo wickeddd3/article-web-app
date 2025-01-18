bf3c25ec5365e34a6d4e5e0f999c4af2df9ee990
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:51:53 +0800

    feat: add SidebarMenu component

---
+++ src/components/layouts/sidebar/SidebarMenu.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SidebarMenuList from "@/components/layouts/sidebar/SidebarMenuList.vue";
+</script>
+
+<template>
+  <aside class="sidebar-menu">
+    <SidebarMenuList />
+  </aside>
+</template>
