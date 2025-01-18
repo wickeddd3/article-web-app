1e88e0a78c5c20b03781c0f2ecceddac5fbed3c5
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:50:01 +0800

    feat: add LoginPage login form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,4 +1,42 @@
+<script setup lang="ts">
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import { LoginSchema } from "@/schema/auth";
+import TextField from "@/components/input/TextField.vue";
+
+const { handleSubmit } = useForm({
+  validationSchema: toTypedSchema(LoginSchema),
+  initialValues: {
+    email: "",
+    password: "",
+  },
+});
+
+const onSubmit = handleSubmit((values) => {
+  console.log(values);
+});
+</script>
+
 <template>
-  <div>login page</div>
+  <div class="container">
+    <h1>Login</h1>
+    <form aria-labelledby="login-form" @submit="onSubmit">
+      <div>
+        <TextField
+          name="email"
+          label="Email Address"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div>
+        <TextField
+          name="password"
+          label="Password"
+          placeholder="••••••••"
+          type="password"
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
-<script setup lang="ts"></script>
