10384f00d0ce656bf5ad81e3f3e3178a4f678cc9
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 23:55:41 +0800

    feat: add localStorageAuthUser and renamed tokenName to localStorageTokenName

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,3 +1,5 @@
 export const baseURL = import.meta.env.VITE_APP_API_BASE_URL || "";
-export const tokenName =
+export const localStorageTokenName =
   import.meta.env.VITE_APP_TOKEN_LOCAL_STORAGE_NAME || "";
+export const localStorageAuthUser =
+  import.meta.env.VITE_APP_USER_LOCAL_STORAGE_NAME || "";
