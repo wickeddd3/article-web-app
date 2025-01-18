a94beec403aaf3f899d28d2d03f049f8bb55e460
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:49:38 +0800

    feat: add TextField component

---
+++ src/components/input/TextField.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useField } from "vee-validate";
+
+export type TextFieldProps = {
+  label?: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  disabled?: boolean;
+};
+
+const props = withDefaults(defineProps<TextFieldProps>(), {
+  label: "",
+  type: "text",
+  placeholder: "",
+  disabled: false,
+});
+
+if (!props.name) {
+  throw new Error("The `name` prop is required for TextField.");
+}
+
+const { value, errorMessage } = useField(props.name);
+</script>
+
+<template>
+  <label :for="props.name">{{ props.label }}</label>
+  <input
+    :id="props.name"
+    v-model="value"
+    :type="props.type"
+    :placeholder="props.placeholder"
+    :disabled="props.disabled"
+    :autocomplete="props.name"
+  />
+  <span v-if="errorMessage">{{ errorMessage }}</span>
+</template>
