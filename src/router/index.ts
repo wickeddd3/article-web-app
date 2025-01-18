f31cc2322588417dce4811083c2e59e2fe9416f7
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 23:56:46 +0800

    feat: use getLocalStorageState and localStorageTokenName instead of hardcoding token name

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,15 @@
 import * as vR from "vue-router";
 import { routes } from "@/router/routes";
+import { getLocalStorageState } from "@/utils/local-storage";
+import { localStorageTokenName } from "@/config/app.config";
 
 const router = vR.createRouter({
   history: vR.createWebHistory(), // WebHash  history will add hash before every route
-  routes, // Send the routes here.
+  routes,
 });
 
 router.beforeEach((to, _from, next) => {
-  const token = localStorage.getItem("article.accessToken");
+  const token = getLocalStorageState(localStorageTokenName);
 
   if (to.meta.authOnly && !token) next({ name: "login" });
   else if (to.meta.guestOnly && token) next({ name: "dashboard" });
