ed52ae95a6fdcd89d20a856426be1c58c3ecd72d
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 06:16:58 +0800

    feat: add companies store

---
+++ src/stores/companies.ts
@@ -0,0 +1,78 @@
+import { defineStore } from "pinia";
+import type { CompaniesState } from "@/types/company";
+import type {
+  CompanySchemaType,
+  EditCompanySchemaType,
+} from "@/schema/company";
+import { create, find, list, update } from "@/services/companies";
+import { fileToDataURL } from "@/utils/file";
+
+export const useCompaniesStore = defineStore("companies", {
+  state: (): CompaniesState => ({
+    companies: {
+      data: [],
+      loading: false,
+    },
+    addCompanyForm: {
+      loading: false,
+    },
+  }),
+  getters: {
+    companiesData: (state) => state.companies.data,
+  },
+  actions: {
+    async fetchCompanies() {
+      const { data, status } = await list();
+      if (status === 200) {
+        this.companies.data = data;
+        return true;
+      }
+      return false;
+    },
+    async createCompany(formData: CompanySchemaType) {
+      this.addCompanyForm.loading = true;
+      const data = {
+        ...formData,
+        logo: formData.logo ? await fileToDataURL(formData.logo || "") : "",
+      };
+      const { data: newCompany, status } = await create(data);
+      if (status === 201) {
+        const companies = [...this.companies.data, newCompany];
+        this.companies.data = companies;
+        this.addCompanyForm.loading = false;
+        return true;
+      }
+      this.addCompanyForm.loading = false;
+      return false;
+    },
+    async fetchCompany(id: number) {
+      const { data, status } = await find(id);
+      if (status === 200) {
+        return data;
+      }
+      return false;
+    },
+    async updateCompany(formData: EditCompanySchemaType, id: number) {
+      this.addCompanyForm.loading = true;
+      const data = {
+        ...formData,
+        newLogo: formData.newLogo
+          ? await fileToDataURL(formData.newLogo || "")
+          : "",
+      };
+      const { data: updatedCompany, status } = await update(data, id);
+      if (status === 200) {
+        const companies = [...this.companies.data];
+        const updatedCompanyIndex = companies.findIndex(
+          (company) => Number(company.id) === id
+        );
+        companies[updatedCompanyIndex] = updatedCompany;
+        this.companies.data = companies;
+        this.addCompanyForm.loading = false;
+        return true;
+      }
+      this.addCompanyForm.loading = false;
+      return false;
+    },
+  },
+});
