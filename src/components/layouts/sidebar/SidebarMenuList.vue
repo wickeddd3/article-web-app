06eae04bc2a935fafc84ff0c283f920a601656df
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:52:02 +0800

    feat: add SideBarMenuList component

---
+++ src/components/layouts/sidebar/SidebarMenuList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SidebarMenuListItem from "@/components/layouts/sidebar/SidebarMenuListItem.vue";
+import { sidebarMenu } from "@/components/layouts/sidebar/sidebar-menu.ts";
+</script>
+
+<template>
+  <div class="sidebar-menu-list">
+    <SidebarMenuListItem
+      v-for="item in sidebarMenu"
+      :key="item.title"
+      :title="item.title"
+      :link="item.link"
+    />
+  </div>
+</template>
