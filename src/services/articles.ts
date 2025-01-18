f1c3babe744ff825d6022d12638c68071acf4d25
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 15:52:45 +0800

    feat: add articles service

---
+++ src/services/articles.ts
@@ -0,0 +1,50 @@
+import { apiRequest } from "@/lib/axios";
+import { type ArticleSchemaType } from "@/schema/article";
+import { AxiosError } from "axios";
+
+const baseUrl = "/api/articles";
+const articlesResource = apiRequest({ url: baseUrl });
+
+export const list = async () => {
+  try {
+    return await articlesResource.get();
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
+};
+
+export const create = async (data: ArticleSchemaType) => {
+  try {
+    return await articlesResource.post(data);
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
+};
+
+export const update = async (data: ArticleSchemaType, id: number) => {
+  try {
+    return await articlesResource.put(data, { url: `${baseUrl}/${id}` });
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
+};
+
+export const remove = async (id: number) => {
+  try {
+    return await articlesResource.delete({ url: `${baseUrl}/${id}` });
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      return error.response;
+    }
+    throw new Error("An unexpected error occurred");
+  }
+};
