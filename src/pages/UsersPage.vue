a37c00ba8c3052cbed1547c083b6d2594d36d9a2
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 03:40:48 +0800

    feat: fetch and show user list with create user and edit user link

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,4 +1,21 @@
+<script setup lang="ts">
+import { useUsersStore } from "@/stores/users";
+import { onMounted, computed } from "vue";
+
+const usersStore = useUsersStore();
+const users = computed(() => usersStore.usersData);
+
+onMounted(() => usersStore.fetchUsers());
+</script>
+
 <template>
-  <div>users page</div>
+  <div>
+    <RouterLink :to="`/users/create`">Create new user</RouterLink>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ `${user.firstname} ${user.lastname}` }}
+        <RouterLink :to="`/users/${user.id}/edit`">Edit</RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
-<script setup lang="ts"></script>
