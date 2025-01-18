50cf2ef64057693574c3db67faad3765e939d648
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 06:18:10 +0800

    feat: fetch and show company list

--- src/pages/CompaniesPage.vue
+++ src/pages/CompaniesPage.vue
@@ -1,4 +1,21 @@
+<script setup lang="ts">
+import { useCompaniesStore } from "@/stores/companies";
+import { onMounted, computed } from "vue";
+
+const companiesStore = useCompaniesStore();
+const companies = computed(() => companiesStore.companiesData);
+
+onMounted(() => companiesStore.fetchCompanies());
+</script>
+
 <template>
-  <div>companies page</div>
+  <div>
+    <RouterLink :to="`/companies/create`">Create new company</RouterLink>
+    <ul>
+      <li v-for="company in companies" :key="company.id">
+        {{ company.name }}
+        <RouterLink :to="`/companies/${company.id}/edit`">Edit</RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
-<script setup lang="ts"></script>
