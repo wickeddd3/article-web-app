b17bf03004001a2ef2170a5b701d146fff4f3739
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:55:08 +0800

    feat: add MainLayout as component of '/' base path and redirect to /dashboard when '/' path is visited

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,44 +1,48 @@
 import * as vR from "vue-router";
 
 export const routes: vR.RouteRecordRaw[] = [
-  {
-    path: "/dashboard",
-    component: () => import("@/pages/DashboardPage.vue"),
-    name: "dashboard",
-    meta: {
-      authOnly: true,
-    },
-  },
-  {
-    path: "/all-media",
-    component: () => import("@/pages/AllMediaPage.vue"),
-    name: "all-media",
-    meta: {
-      authOnly: true,
-    },
-  },
-  {
-    path: "/users",
-    component: () => import("@/pages/UsersPage.vue"),
-    name: "users",
-    meta: {
-      authOnly: true,
-    },
-  },
-  {
-    path: "/companies",
-    component: () => import("@/pages/CompaniesPage.vue"),
-    name: "companies",
-    meta: {
-      authOnly: true,
-    },
-  },
+  // Redirect from "/" to "/dashboard"
   {
     path: "/",
+    redirect: "/dashboard",
+  },
+  // Parent route for all `authOnly` routes
+  {
+    path: "/",
+    component: () => import("@/components/layouts/MainLayout.vue"), // Main layout
+    meta: { authOnly: true },
+    children: [
+      {
+        path: "dashboard",
+        component: () => import("@/pages/DashboardPage.vue"),
+        name: "dashboard",
+        meta: { authOnly: true },
+      },
+      {
+        path: "all-media",
+        component: () => import("@/pages/AllMediaPage.vue"),
+        name: "all-media",
+        meta: { authOnly: true },
+      },
+      {
+        path: "users",
+        component: () => import("@/pages/UsersPage.vue"),
+        name: "users",
+        meta: { authOnly: true },
+      },
+      {
+        path: "companies",
+        component: () => import("@/pages/CompaniesPage.vue"),
+        name: "companies",
+        meta: { authOnly: true },
+      },
+    ],
+  },
+  // Route for guest-only pages
+  {
+    path: "/login",
     component: () => import("@/pages/LoginPage.vue"),
     name: "login",
-    meta: {
-      guestOnly: true,
-    },
+    meta: { guestOnly: true },
   },
 ];
