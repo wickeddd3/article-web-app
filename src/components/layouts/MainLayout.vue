0c82d6b4fc15dd829225c4b9f3866ba7bbfa4b7a
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:51:42 +0800

    feat: add MainLayout component

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SidebarMenu from "@/components/layouts/sidebar/SidebarMenu.vue";
+</script>
+
+<template>
+  <main class="main-layout">
+    <SidebarMenu />
+    <div class="main-layout__content">
+      <RouterView />
+    </div>
+  </main>
+</template>
