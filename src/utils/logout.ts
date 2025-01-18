d6bb7def1a0ddb19e105c47c59d1675f350a9887
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 23:57:06 +0800

    feat: remove local storage auth user on logout

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,9 +1,13 @@
-import { tokenName } from "@/config/app.config";
+import {
+  localStorageAuthUser,
+  localStorageTokenName,
+} from "@/config/app.config";
 import { removeLocalStorageState } from "@/utils/local-storage";
 
 export const logout = () => {
   // remove token from local storage
-  removeLocalStorageState(tokenName);
+  removeLocalStorageState(localStorageTokenName);
+  removeLocalStorageState(localStorageAuthUser);
   // reload and redirect to homepage
   const hostUrl = `${window.location.protocol}//${window.location.host}/login`;
   window.location.href = hostUrl;
