294d558c4cb2e9f3e3f1e83b43ada1f48faa3ba6
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:52:38 +0800

    feat: add SidebarMenuListItem component

---
+++ src/components/layouts/sidebar/SidebarMenuListItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+export type SidebarMenuListItemProps = {
+  title: string;
+  link: string;
+};
+
+const props = defineProps<SidebarMenuListItemProps>();
+
+if (!props.title) {
+  throw new Error("The `title` prop is required for SidebarMenuListItem.");
+}
+
+if (!props.link) {
+  throw new Error("The `link` prop is required for SidebarMenuListItem.");
+}
+</script>
+
+<template>
+  <div class="sidebar-menu-list__item">
+    <RouterLink :to="props.link">{{ props.title }}</RouterLink>
+  </div>
+</template>
