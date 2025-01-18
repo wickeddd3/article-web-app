26bde35d1829e8c4a9d3500d12da6f0c36c7e739
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 21:53:10 +0800

    feat: add sidebar menu links

---
+++ src/components/layouts/sidebar/sidebar-menu.ts
@@ -0,0 +1,18 @@
+export const sidebarMenu = [
+  {
+    title: "Dashboard",
+    link: "/dashboard",
+  },
+  {
+    title: "All Media",
+    link: "/all-media",
+  },
+  {
+    title: "Companies",
+    link: "/companies",
+  },
+  {
+    title: "Users",
+    link: "/users",
+  },
+];
