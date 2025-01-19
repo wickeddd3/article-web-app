4f58da22ebdd512fd9e01dac33f54e6e69e2afaf
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 01:11:33 +0800

    feat: add getCurrentDateString util

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,3 +6,12 @@ export const isoStringToDate = (isoString: string) => {
   const formattedDate = dateObj.toISOString().split("T")[0];
   return formattedDate;
 };
+
+export const getCurrentDateString = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
