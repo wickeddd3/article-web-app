ec6107e5285563be85402f30370128665123582b
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 00:57:19 +0800

    feat: show ArticleList only if has articles

--- src/components/articles/ArticleList.vue
+++ src/components/articles/ArticleList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Article } from "@/types/article";
 import ArticleListItem from "@/components/articles/ArticleListItem.vue";
+import { computed } from "vue";
 
 export type ArticleListProps = {
   title?: string;
@@ -15,10 +16,12 @@ const props = withDefaults(defineProps<ArticleListProps>(), {
 if (!props.articles) {
   throw new Error("The `articles` prop is required for ArticleList.");
 }
+
+const hasArticles = computed(() => props.articles.length > 0);
 </script>
 
 <template>
-  <div>
+  <div v-if="hasArticles">
     <h1 v-if="title">{{ title }}</h1>
     <ul>
       <ArticleListItem
