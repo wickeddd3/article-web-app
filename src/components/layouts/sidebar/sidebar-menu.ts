9b9e9a3c9a330770510647ecde2dad143161bd93
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 02:43:31 +0800

    feat: add access property

--- src/components/layouts/sidebar/sidebar-menu.ts
+++ src/components/layouts/sidebar/sidebar-menu.ts
@@ -2,17 +2,21 @@ export const sidebarMenu = [
   {
     title: "Dashboard",
     link: "/dashboard",
+    access: ["Editor", "Writer"],
   },
   {
     title: "All Media",
     link: "/all-media",
+    access: ["Editor", "Writer"],
   },
   {
     title: "Companies",
     link: "/companies",
+    access: ["Editor"],
   },
   {
     title: "Users",
     link: "/users",
+    access: ["Editor"],
   },
 ];
