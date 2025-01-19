fed834ec1f26b4e92374834d7080c2ee38d1a778
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 00:17:53 +0800

    feat: add ArticleList component

---
+++ src/components/articles/ArticleList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Article } from "@/types/article";
+import ArticleListItem from "@/components/articles/ArticleListItem.vue";
+
+export type ArticleListProps = {
+  title?: string;
+  articles: Article[];
+};
+
+const props = withDefaults(defineProps<ArticleListProps>(), {
+  title: "",
+  articles: () => [],
+});
+
+if (!props.articles) {
+  throw new Error("The `articles` prop is required for ArticleList.");
+}
+</script>
+
+<template>
+  <div>
+    <h1 v-if="title">{{ title }}</h1>
+    <ul>
+      <ArticleListItem
+        v-for="article in articles"
+        :key="article.id"
+        :article="article"
+      />
+    </ul>
+  </div>
+</template>
