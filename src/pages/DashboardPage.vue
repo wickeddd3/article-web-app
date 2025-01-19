a0bf1ee2ac2b4fbab9d443b3286edbd3e86022d0
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 17:28:25 +0800

    feat: fetch and show articles

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,4 +1,21 @@
+<script setup lang="ts">
+import { useArticlesStore } from "@/stores/articles";
+import { onMounted, computed } from "vue";
+
+const articlesStore = useArticlesStore();
+const articles = computed(() => articlesStore.articlesData);
+
+onMounted(() => articlesStore.fetchArticles());
+</script>
+
 <template>
-  <div>dashboard page</div>
+  <div>
+    <RouterLink :to="`/articles/create`">Create new article</RouterLink>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        {{ article.title }}
+        <RouterLink :to="`/articles/${article.id}/edit`">Edit</RouterLink>
+      </li>
+    </ul>
+  </div>
 </template>
-<script setup lang="ts"></script>
