c60b9d8087499be380fecb43142b7ca26e730cab
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 03:19:15 +0800

    feat: add login page class

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -29,29 +29,31 @@ const onSubmit = handleSubmit(async (values) => {
 </script>
 
 <template>
-  <div class="container">
-    <h1>Login</h1>
-    <form aria-labelledby="login-form" @submit="onSubmit">
-      <div>
-        <TextField
-          name="email"
-          label="Email Address"
-          placeholder="Enter your email"
-          :disabled="loading"
-        />
-      </div>
-      <div>
-        <TextField
-          name="password"
-          label="Password"
-          placeholder="••••••••"
-          type="password"
-          :disabled="loading"
-        />
-      </div>
-      <button type="submit" :disabled="loading">
-        {{ loading ? "Loading..." : "Login" }}
-      </button>
-    </form>
+  <div class="login-page">
+    <div class="login-page__wrapper">
+      <h1>Login</h1>
+      <form aria-labelledby="login-form" @submit="onSubmit">
+        <div>
+          <TextField
+            name="email"
+            label="Email Address"
+            placeholder="Enter your email"
+            :disabled="loading"
+          />
+        </div>
+        <div>
+          <TextField
+            name="password"
+            label="Password"
+            placeholder="••••••••"
+            type="password"
+            :disabled="loading"
+          />
+        </div>
+        <button type="submit" :disabled="loading">
+          {{ loading ? "Loading..." : "Login" }}
+        </button>
+      </form>
+    </div>
   </div>
 </template>
