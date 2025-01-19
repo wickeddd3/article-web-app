6a5565cb45ce2e4affbdbfaa3896493fdda29894
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 00:18:29 +0800

    feat: return empty string is isoString is falsy

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,5 @@
 export const isoStringToDate = (isoString: string) => {
+  if (!isoString) return "";
   // Convert to a Date object
   const dateObj = new Date(isoString);
   // Extract the date in `YYYY-MM-DD` format
