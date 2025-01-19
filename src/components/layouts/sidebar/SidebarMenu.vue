619810f379b802531d38451facd8121cd0902234
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-20 02:50:10 +0800

    feat: add logout button

--- src/components/layouts/sidebar/SidebarMenu.vue
+++ src/components/layouts/sidebar/SidebarMenu.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import SidebarMenuList from "@/components/layouts/sidebar/SidebarMenuList.vue";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+const logout = () => {
+  authStore.logoutUser();
+};
 </script>
 
 <template>
   <aside class="sidebar-menu">
     <SidebarMenuList />
+    <button @click="logout">Log Out</button>
   </aside>
 </template>
