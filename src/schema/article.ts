06473dc556211ed26bf85970f2d0e7b5e2d0f845
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 17:26:52 +0800

    feat: update ArticleSchema and add EditArticleSchema

--- src/schema/article.ts
+++ src/schema/article.ts
@@ -1,27 +1,81 @@
 import { z } from "zod";
 
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+
 export const ArticleSchema = z.object({
-  companyId: z.string({ required_error: "Company is required" }),
+  companyId: z
+    .number({ required_error: "Company is required" })
+    .min(1, { message: "Company is required" }),
+  writerId: z
+    .number({ required_error: "Writer is required" })
+    .min(1, { message: "Writer is required" }),
   title: z
     .string({ required_error: "Title is required" })
     .trim()
     .min(1, { message: "Title is required" }),
-  link: z.string({ required_error: "Link is required" }).url(),
-  date: z.string({ required_error: "Date is required" }).datetime(),
+  link: z
+    .string({ required_error: "Link is required" })
+    .trim()
+    .min(1, { message: "Link is required" })
+    .url(),
+  date: z
+    .string()
+    .nonempty("Date is required")
+    .refine((value) => !isNaN(Date.parse(value)), "Invalid date format"),
   content: z
     .string({ required_error: "Content is required" })
     .trim()
     .min(1, { message: "Content is required" }),
+  status: z.enum(["ForEdit", "Published"]),
   image: z
-    .custom<File>((value) => value instanceof File, {
-      message: "Invalid file. Please upload a valid file.",
+    .instanceof(File)
+    .refine((file) => file.size > 0, {
+      message: "Image is required and must be a valid file",
     })
-    .refine((file) => file.type.startsWith("image/"), {
-      message: "Only image files are allowed.",
-    })
-    .refine((file) => file.size <= 5 * 1024 * 1024, {
-      message: "File size must not exceed 5MB.",
-    }),
+    .refine((file) => file.size <= MAX_FILE_SIZE, "File size must be under 5MB")
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      ".jpg, .jpeg, and .png files are accepted."
+    ),
 });
 
 export type ArticleSchemaType = z.infer<typeof ArticleSchema>;
+
+export const EditArticleSchema = z.object({
+  id: z.number(),
+  companyId: z
+    .number({ required_error: "Company is required" })
+    .min(1, { message: "Company is required" }),
+  title: z
+    .string({ required_error: "Title is required" })
+    .trim()
+    .min(1, { message: "Title is required" }),
+  link: z
+    .string({ required_error: "Link is required" })
+    .trim()
+    .min(1, { message: "Link is required" })
+    .url(),
+  date: z
+    .string()
+    .nonempty("Date is required")
+    .refine((value) => !isNaN(Date.parse(value)), "Invalid date format"),
+  content: z
+    .string({ required_error: "Content is required" })
+    .trim()
+    .min(1, { message: "Content is required" }),
+  status: z.enum(["ForEdit", "Published"]),
+  newImage: z
+    .instanceof(File)
+    .refine((file) => file.size > 0, {
+      message: "Image is required and must be a valid file",
+    })
+    .refine((file) => file.size <= MAX_FILE_SIZE, "File size must be under 5MB")
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      ".jpg, .jpeg, and .png files are accepted."
+    )
+    .optional(),
+});
+
+export type EditArticleSchemaType = z.infer<typeof EditArticleSchema>;
